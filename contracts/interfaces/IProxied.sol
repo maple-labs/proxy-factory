9d9f98ac7eb577d0682f4f5f9d0290774c44541c
Michael De Luca <35537333+deluca-mike@users.noreply.github.com>
2021-09-08 13:32:10 -0400

    refactor: Simpler, more functional, and less opinionated (#7)
    * refactor: Simpler, more functional, and less opinionated
    
    - use correct ds-test
    - use correct docker image for ci
    - remove recommended version
    - simplify implementation registration by treating an initializer exactly like a migrator
    - create test account
    - update tests and add test for new instance with no initializer and upgrade with no migrator
    - prevent deleting migrator as opinionated layer
    - removed all opinionated requires and moved non-opinionated requires to internals
    - initializers are migrators to and from same version number, freeing up version 0, and resulting in less checks needed
    
    * refactor: remove most external interfaces
    
    * refactor: removal all external interfaces
    
    - create2
    - decouple upgrade from migrate
    - remove need for proxy to call factory for its own implementation
    
    * fix: ci test
    
    Co-authored-by: JG Carvalho <jg@jgcarv.com>

--- contracts/interfaces/IProxied.sol
+++ contracts/interfaces/IProxied.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/// @title A proxied implementation must be Proxied.
 interface IProxied {
 
     /// @notice Returns the address of the proxy factory.
@@ -9,7 +10,10 @@ interface IProxied {
     /// @notice Returns the address of the implementation contract.
     function implementation() external view returns (address);
 
-    /// @notice Modifies the proxy's storage by delegatecalling a migrator contract with some arguments.
+    /// @notice Modifies the proxy's implementation address.
+    function setImplementation(address newImplementation) external;
+
+    /// @notice Modifies the proxy's storage by delegate-calling a migrator contract with some arguments.
     function migrate(address migrator, bytes calldata arguments) external;
 
 }
