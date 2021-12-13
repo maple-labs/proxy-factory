ccae18206154f70ae81239b7e39f8cd18427ce96
Michael De Luca <michael.deluca@circle-free.com>
2021-12-13 14:34:57 -0500

    chore: natspec and comments

--- contracts/ProxyFactory.sol
+++ contracts/ProxyFactory.sol
@@ -14,20 +14,28 @@ contract ProxyFactory {
 
     mapping(uint256 => mapping(uint256 => address)) internal _migratorForPath;
 
+    /// @notice Returns the implementation of `proxy_`.
     function _getImplementationOfProxy(address proxy_) private view returns (bool success_, address implementation_) {
         bytes memory returnData;
+        // Since `_getImplementationOfProxy` is a private function, no need to check `proxy_` is a contract.
         ( success_, returnData ) = proxy_.staticcall(abi.encodeWithSelector(IProxied.implementation.selector));
         implementation_ = abi.decode(returnData, (address));
     }
 
+    /// @notice Initializes `proxy_` using the initializer for `version_`, given some initialization arguments.
     function _initializeInstance(address proxy_, uint256 version_, bytes memory arguments_) private returns (bool success_) {
+        // The migrator, where fromVersion == toVersion, is an initializer.
         address initializer = _migratorForPath[version_][version_];
 
+        // If there is no initializer, then no initialization is necessary, so long as no initialization arguments were provided.
         if (initializer == address(0)) return arguments_.length == uint256(0);
 
+        // Call the migrate function on the proxy, passing any initialization arguments.
+        // Since `_initializeInstance` is a private function, no need to check `proxy_` is a contract.
         ( success_, ) = proxy_.call(abi.encodeWithSelector(IProxied.migrate.selector, initializer, arguments_));
     }
 
+    /// @notice Deploys a new proxy for some version, with some initialization arguments, using `create` (i.e. factory's nonce determines the address).
     function _newInstance(uint256 version_, bytes memory arguments_) internal virtual returns (bool success_, address proxy_) {
         address implementation = _implementationOf[version_];
 
@@ -37,17 +45,25 @@ contract ProxyFactory {
         success_ = _initializeInstance(proxy_, version_, arguments_);
     }
 
+    /// @notice Deploys a new proxy, with some initialization arguments, using `create2` (i.e. salt determines the address).
+    /// @dev    This factory needs to be IDefaultImplementationBeacon, since the proxy will pull its implementation from it.
     function _newInstance(bytes memory arguments_, bytes32 salt_) internal virtual returns (bool success_, address proxy_) {
         proxy_ = address(new Proxy{ salt: salt_ }(address(this), address(0)));
 
+        // Fetch the implementation from the proxy. Don't care about success, since the version of the implementation will be checked in the nest step.
         ( , address implementation ) = _getImplementationOfProxy(proxy_);
 
+        // Get the version of the implementation.
         uint256 version = _versionOf[implementation];
 
+        // Successful if version is nonzero (i.e. implementation fetched successfully from proxy) and initializing the instance succeeds.
         success_ = (version != uint256(0)) && _initializeInstance(proxy_, version, arguments_);
     }
 
+    /// @notice Registers an implementation for some version.
     function _registerImplementation(uint256 version_, address implementation_) internal virtual returns (bool success_) {
+        // Version 0 is not allowed since its the default value of all _versionOf[implementation_].
+        // Implementation cannot already be registered and cannot be empty account (and thus also not address(0)).
         if (
             version_ == uint256(0) ||
             _implementationOf[version_] != address(0) ||
@@ -55,15 +71,19 @@ contract ProxyFactory {
             !_isContract(implementation_)
         ) return false;
 
+        // Store in two-way mappings.
         _implementationOf[version_] = implementation_;
         _versionOf[implementation_] = version_;
 
         return true;
     }
 
+    /// @notice Registers a migrator for between two versions. If `fromVersion_ == toVersion_`, migrator is an initializer.
     function _registerMigrator(uint256 fromVersion_, uint256 toVersion_, address migrator_) internal virtual returns (bool success_) {
+        // Version 0 is invalid.
         if (fromVersion_ == uint256(0) || toVersion_ == uint256(0)) return false;
 
+        // Migrator must either be zero (clearing) or a contract (setting).
         if (migrator_ != address(0) && !_isContract(migrator_)) return false;
 
         _migratorForPath[fromVersion_][toVersion_] = migrator_;
@@ -71,29 +91,39 @@ contract ProxyFactory {
         return true;
     }
 
+    /// @notice Upgrades a proxy to a new version of an implementation, with some migration arguments.
+    /// @dev    Inheritor should revert on `success_ = false`, since proxy can be set to new implementation, but failed to migrate.
     function _upgradeInstance(address proxy_, uint256 toVersion_, bytes memory arguments_) internal virtual returns (bool success_) {
+        // Check that the proxy is currently a contract, just once, ahead of the 3 times it will be low-level-called.
         if (!_isContract(proxy_)) return false;
 
         address toImplementation = _implementationOf[toVersion_];
 
+        // The implementation being migrated must have been registered (which also implies that `toVersion_` was not 0).
         if (toImplementation == address(0)) return false;
 
+        // Fetch the implementation from the proxy.
         address fromImplementation;
         ( success_, fromImplementation ) = _getImplementationOfProxy(proxy_);
 
         if (!success_) return false;
 
+        // Set the proxy's implementation.
         ( success_, ) = proxy_.call(abi.encodeWithSelector(IProxied.setImplementation.selector, toImplementation));
 
         if (!success_) return false;
 
+        // Get the version of the `fromImplementation`, then get the `migrator` of the upgrade path to `toVersion_`.
         address migrator = _migratorForPath[_versionOf[fromImplementation]][toVersion_];
 
+        // If there is no migrator, then no migration is necessary, so long as no migration arguments were provided.
         if (migrator == address(0)) return arguments_.length == uint256(0);
 
+        // Call the migrate function on the proxy, passing any migration arguments.
         ( success_, ) = proxy_.call(abi.encodeWithSelector(IProxied.migrate.selector, migrator, arguments_));
     }
 
+    /// @notice Returns the deterministic address of a proxy given some salt.
     function _getDeterministicProxyAddress(bytes32 salt_) internal virtual view returns (address deterministicProxyAddress_) {
         // See https://docs.soliditylang.org/en/v0.8.7/control-structures.html#salted-contract-creations-create2
         return address(
@@ -112,6 +142,7 @@ contract ProxyFactory {
         );
     }
 
+    /// @notice Returns whether the account is currently a contract.
     function _isContract(address account_) internal view returns (bool isContract_) {
         uint256 size;
 
